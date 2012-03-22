6466c2d97bee1737209e4004a8b50399ea4252eb
ngadiono <cristiano_ondoel@yahoo.com>
2012-03-22 23:48:37 +0700

    selesai membuat hero unit

--- application/views/frontend/element/vheroUnit.php
+++ application/views/frontend/element/vheroUnit.php
@@ -1,18 +1,15 @@
 <div class="hero-unit">
     <div class="container">
         <div class="row">
-            <div class="span13">
-                <h3>
+            <div class="span9">
+                <h1>
                     Web application programming platform.
                     <br/>
                     Event-driven, truly non-blocking, open source.
-                </h3>
+                </h1>
             </div>
             <div class="span3">
-                <a class="btn-primary large pull-right" href="/get.xmlt" target="_top">
-                    <span class="icon-white"/>
-                    Download Opa
-                </a>
+                <a href="#" class="btn btn-large btn-primary pull-right tombol">Download</a>
             </div>
         </div>
     </div>
