b311f23b8c10dad2254dd0ff26b2d4a94bb1fda5
ngadiono <cristiano_ondoel@yahoo.com>
2012-03-23 01:18:41 +0700

    list content jadi

--- application/views/frontend/element/vcontent.php
+++ application/views/frontend/element/vcontent.php
@@ -14,51 +14,5 @@
         </div>
     </div>
     <hr/>
-    <div class="row">
-        <div class="span-one-third">
-            <h2>What is Opa?</h2>
-            <ul class="media-grid">
-                <li>
-                    <a href="/about.xmlt">
-                        <img src="/img/opatour.jpg" alt="Opa tour"/>
-                    </a>
-                </li>
-            </ul>
-            <p>
-                Opa is the new generation of Web development language that allows users to write distributed web applications using a single technology.
-                <br/>
-                <a href="/about.xmlt">Learn more ›</a>
-            </p>
-        </div>
-        <div class="span-on">
-            <h2>Why Opa?</h2>
-            <ul class="media-grid">
-                <li>
-                    <a href="/why_opa.xmlt">
-                        <img src="/img/apps-300.png" alt="Apps"/>
-                    </a>
-                </li>
-            </ul>
-            <p>
-                Opa Web development platform is well-suited for building distributed Web applications and services as well as games!
-                <br/>
-                <a href="/why_opa.xmlt">Learn more ›</a>
-            </p>
-        </div>
-        <div class="span-one-third">
-            <h2>News & Events</h2>
-            <h6>A talk on the Opa support for MongoDB</h6>
-            <h5>March 21, 2012 7:45PM</h5>
-            <p>
-                MongoDB Paris Meetup #4 |
-                <a href="http://www.meetup.com/Paris-MongoDB-User-Group/events/53020022/?a=ea1_lnm&rv=ea1" target="_blank">Subscribe now ›</a>
-            </p>
-            <h6>DZone.com</h6>
-            <h5>Opa Aims to Rethink Development for the Cloud</h5>
-            <p>
-                Will Opa become the cool language of the day for web apps? Ben Kepes thinks it's on the right track |
-                <a href="http://cloud.dzone.com/articles/opa-aims-rethink-development" target="_blank">Read ›</a>
-            </p>
-        </div>
-    </div>
+        <?php $this->load->view('frontend/listContent/vlist'); ?>
 </div>
---
+++ application/views/frontend/listContent/vlist.php
@@ -0,0 +1,45 @@
+<div class="row">
+    <div class="span4">
+        <h2>What is Opa?</h2>
+
+                <a href="/about.xmlt">
+                    <img class="img-content" src="<?php echo base_url(); ?>resource/img/slide/bootstrap-mdo-sfmoma-01.jpg" alt="Opa tour"/>
+                </a>
+
+        <p>
+            Opa is the new generation of Web development language that allows users to write distributed web applications using a single technology.
+            <br/>
+            <a href="/about.xmlt">Learn more &rsaquo;</a>
+        </p>
+    </div>
+    <div class="span4">
+        <h2>Why Opa?</h2>
+        <ul class="media-grid">
+            <li>
+                <a href="/why_opa.xmlt">
+                    <img class="img-content" src="<?php echo base_url(); ?>resource/img/slide/bootstrap-mdo-sfmoma-01.jpg" alt="Apps"/>
+                </a>
+            </li>
+        </ul>
+        <p>
+            Opa Web development platform is well-suited for building distributed Web applications and services as well as games!
+            <br/>
+            <a href="/why_opa.xmlt">Learn more &rsaquo;</a>
+        </p>
+    </div>
+    <div class="span4">
+        <h2>News & Events</h2>
+        <h6>A talk on the Opa support for MongoDB</h6>
+        <h5>March 21, 2012 7:45PM</h5>
+        <p>
+            MongoDB Paris Meetup #4 |
+            <a href="http://www.meetup.com/Paris-MongoDB-User-Group/events/53020022/?a=ea1_lnm&rv=ea1" target="_blank">Subscribe now &rsaquo;</a>
+        </p>
+        <h6>DZone.com</h6>
+        <h5>Opa Aims to Rethink Development for the Cloud</h5>
+        <p>
+            Will Opa become the cool language of the day for web apps? Ben Kepes thinks it's on the right track |
+            <a href="http://cloud.dzone.com/articles/opa-aims-rethink-development" target="_blank">Read &rsaquo;</a>
+        </p>
+    </div>
+</div>
